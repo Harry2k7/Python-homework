ac02010cbc13285c1a3fc6947c8eedbceab06cc9
igor <igorkudriy2k7@gmail.com>
2023-07-18 22:06:28 +0300

    Финальный коммит

--- Task_1_difficult.py
+++ Task_1_difficult.py
@@ -2,20 +2,21 @@
 
 def sum_of_digits(num):
     integer_part = int(num)
-    fractal_part = num - integer_part
-    sum = 0
+    fractal_part = round(num % 1, 6)
+    total_sum = 0
 
     while integer_part > 0:
         digit = integer_part % 10
-        sum += digit
+        total_sum += digit
         integer_part //= 10
 
     for _ in range(6):
         fractal_part *= 10
         digit = int(fractal_part) % 10
-        sum += digit
+        total_sum += digit
         fractal_part -= digit
-    return sum
+
+    return total_sum
 
 num = float(input("Введите любое число: "))
 print(f"Сумма цифр числа {num} равна {sum_of_digits(num)}")
